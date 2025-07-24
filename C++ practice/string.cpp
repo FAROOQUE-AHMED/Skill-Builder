f7b87c40404b660adcb6241d0b410197a376b4da
FAROOQUE AHMED <farooqueahmedsiddique@gmail.com>
2025-07-25 05:21:29 +0530

    added string file

---
+++ C++ practice/string.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main(){
+    char a[]="farooque";
+    // string length
+    // int b=strlen(a);
+    // cout<<b;
+
+    //string revverse
+    // strrev(a);
+    // cout<<a;
+    
+
+    // string concatinate
+    // char c[]=" ahmed";
+    // strcat(a,c);
+    // cout<<a;
+
+    //string merge
+
+    char c[10];
+    strcpy(c,a);
+    cout<<c;
+
+}
