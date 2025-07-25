1e5a661ec12e40cf697ce4c63e3ce39d9b3bd83a
FAROOQUE AHMED <farooqueahmedsiddique@gmail.com>
2025-07-25 16:48:28 +0530

    data types

---
+++ C++ practice/userDefineDatatypes.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+// struct data type -------------------------------------------------------
+
+// struct str
+// {
+//     int roll;
+//     char name[40];
+//     float marks;
+// };
+
+
+// int main(){
+//     struct str a;
+    
+//     cout<<"Enter roll number :";
+//     cin>>a.roll;
+//     cout<<"Enter full name :";
+//     cin>>a.name;
+//     cout<<"Enter full marks out off 800 :";
+//     cin>>a.marks;
+
+//     cout<<a.roll<<" "<<a.name<<" "<<a.marks;
+
+// }
+
+// emum data type ---------------------------------
+
+int main(){
+    enum weekday{
+        monday,tuesday,wednesday,thursday,friday,saturday,sunday
+
+    };
+
+    cout<<sunday<<endl;
+
+    weekday a=wednesday;
+    cout<<a;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
