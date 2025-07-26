7917277c56d8713c5dbb35bbbfa86b0a6ed82e34
FAROOQUE AHMED <farooqueahmedsiddique@gmail.com>
2025-07-26 21:14:06 +0530

    added encaplsulation file

--- C++ practice/encapsulation.cpp
+++ C++ practice/encapsulation.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
-class A{
+class thief{
     private:
     int pin;
     int balance;
-    public:
     string bankName;
     int IFSC;
     int AccountNo;
 
-
+    public:
     void input(){
     pin=1234;
     balance=123456789;
@@ -18,6 +17,7 @@ class A{
     AccountNo=528467391;
     }
 
+    public:
     void output(){
         cout<<bankName<<endl;
         cout<<balance<<endl;
@@ -27,15 +27,16 @@ class A{
     }
 
 };
-int main(){
-    A obj;
-    obj.input();
-    obj.output();
-    cout<<"aryan is trying to acces my account details..."<<endl;
-    cout<<obj.bankName<<endl;
-    // cout<<obj.balance<<endl;
-    // cout<<obj.pin<<endl;
-    cout<<obj.IFSC<<endl;
-    cout<<obj.AccountNo<<endl;
 
+class Police:public thief
+{
+
+};
+
+int main(){
+    thief obj;
+    Police obj2;
+    obj2.input();
+    obj2.output();
+   
 }
