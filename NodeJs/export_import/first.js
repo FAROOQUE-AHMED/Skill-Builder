6fca8a21369d8dd3b2f273f5e3ba1f0fa81bf32d
FAROOQUE AHMED <farooqueahmedsiddique@gmail.com>
2025-07-31 21:02:23 +0530

    added export import files

---
+++ NodeJs/export_import/first.js
@@ -0,0 +1,13 @@
+// const second = require('./second')
+// console.log(second.z()) 
+
+// console.log("helloo");
+// console.log(arr);
+
+const arr = [1, 2, 3, 7, 8];
+var result = arr.filter((items) => {
+
+    return items != 3;
+
+})
+console.log(result);
