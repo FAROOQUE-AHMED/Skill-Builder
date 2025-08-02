369629cc16f23fa6d815a3fb08508618ea251f97
FAROOQUE AHMED <farooqueahmedsiddique@gmail.com>
2025-08-02 23:13:57 +0530

    added commonds

---
+++ NodeJs/MyApi/data.js
@@ -0,0 +1,7 @@
+const data = [
+    { name: "farooque", email: "farooque@gmail.com", contacts: 9999999999 },
+    { name: "farooque ahmed", email: "farooqueahmed@gmail.com" },
+    { name: "farooque ahmed siddique", email: "farooquesiddique@gmail.com" },
+    { name: "ahmed", email: "ahmed@gmail.com" }
+]
+module.exports = data;
